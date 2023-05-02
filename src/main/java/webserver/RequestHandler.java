9d2c1a09e4149245a3e8ea5965a3cb1ed0f9f35a
김소이 <thdl9893@ajou.ac.kr>
2023-05-02 23:15:34 +0900

    3.4.3.2) 요구사항 2 - GET 방식으로 회원가입하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,10 +9,12 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import model.User;
 import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
@@ -28,22 +30,37 @@ public class RequestHandler extends Thread {
 		log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
 				connection.getPort());
 
-		// 요구사항1 - index.html 응답하기 (url 추출부분 리팩토링)
+		// 요구사항1 - index.html 응답하기
 		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
 			// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
 
 			BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
 			String line = br.readLine();
+			log.debug("line : {}", line);
 			if (line == null) {
 				return;
+			}
+
+			String url = HttpRequestUtils.getUrl(line);
+			log.debug("Url : {}", url);
+
+			// 요구사항2 - GET 방식으로 회원가입하기
+			if (url.startsWith("/user/create")) {
+				int index = url.indexOf("?");
+				String queryString = url.substring(index + 1);
+				Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
+				User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+				log.debug("User : {}", user);
+
+				// 회원가입 후 index.html 화면으로 이동
+				url = "/index.html";
 			}
 
 //			while (!"".equals(line)) {
 //				log.debug(line);
 //				line = br.readLine();
 //			}
-			
-			String url = HttpRequestUtils.getUrl(line);
+			log.debug("webapp url : {}", url);
 
 			byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
 
